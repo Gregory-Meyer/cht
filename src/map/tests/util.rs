f4ba83a565ac9d70868528ee357d3028fcb647d5
Gregory Meyer (gregjm) <me@gregjm.dev>
2020-03-05 16:01:36 -0500

    Fix double free bug, some interface redesign (#10)
    * fix double free, change insert_or_modify interface
    
    fixing the double free was surprisingly subtle, and involved a few small
    changes to the algorithm to bring it more in line with Cliff Click's
    2007 lockfree hash table.
    
    first, it is no longer possible for multiple rehashing operations to be
    in flight at once. this is primarily for simplicity's sake, but Click
    claims it's faster this way. next, bucket pointers that have been copied
    from an old table are marked with a tag bit. this prevents writes on the
    old table from overwriting writes on the new table. finally, removal and
    no longer requires the key to be cloned. instead, the value is destroyed
    via MaybeUninit shenanigans and a tombstone bit is set on the bucket
    pointer. the benefit here is that we are able to remove elements without
    forcing a memory allocation on the user.
    
    the interface of the insert_or_modify family of functions was changed to
    require a function that has both key and value parameters. imo there was
    never a solid reason not to do that, but this allows arbitrary data to
    be accessed from the old key before disposing of it.
    
    additionally, the Bucket/BucketArray classes were completely rewritten
    and moved into a module of their own. i am more satisfied with the
    amount of code duplication now, but i believe there is more work to be
    done to unify the implementations of get, insert, and remove. in the
    future, i hope to use this separate module to create a striped hash
    table similar to Java 7's ConcurrentHashMap. this should significantly
    increase write performance.
    
    note that this commit includes an internal remove_if interface, which is
    intended to address #7.
    
    * move map tests into crate::map::tests
    
    * add bucket key/value destructor tests
    
    ensure that destructors are called for keys and values in a timely
    manner
    
    * correct growth policy to grow by a factor of 2
    
    previous behavior would result in growing by a factor of 4 (!)

---
+++ src/map/tests/util.rs
@@ -0,0 +1,132 @@
+// MIT License
+//
+// Copyright (c) 2020 Gregory Meyer
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+use std::{
+    borrow::{Borrow, BorrowMut},
+    hash::{Hash, Hasher},
+    ops::{Deref, DerefMut},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+};
+
+#[derive(Debug)]
+pub(crate) struct NoisyDropper<T: ?Sized> {
+    parent: Arc<DropNotifier>,
+    pub elem: T,
+}
+
+impl<T> NoisyDropper<T> {
+    pub(crate) fn new(parent: Arc<DropNotifier>, elem: T) -> Self {
+        Self { parent, elem }
+    }
+}
+
+impl<T: ?Sized> Drop for NoisyDropper<T> {
+    fn drop(&mut self) {
+        assert_eq!(self.parent.dropped.swap(true, Ordering::Relaxed), false);
+    }
+}
+
+impl<T: ?Sized + PartialEq> PartialEq for NoisyDropper<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.elem == other.elem
+    }
+}
+
+impl<T: ?Sized + PartialEq> PartialEq<T> for NoisyDropper<T> {
+    fn eq(&self, other: &T) -> bool {
+        &self.elem == other
+    }
+}
+
+impl<T: ?Sized + Eq> Eq for NoisyDropper<T> {}
+
+impl<T: ?Sized + Hash> Hash for NoisyDropper<T> {
+    fn hash<H: Hasher>(&self, hasher: &mut H) {
+        self.elem.hash(hasher);
+    }
+}
+
+impl<T: ?Sized> AsRef<T> for NoisyDropper<T> {
+    fn as_ref(&self) -> &T {
+        &self.elem
+    }
+}
+
+impl<T: ?Sized> AsMut<T> for NoisyDropper<T> {
+    fn as_mut(&mut self) -> &mut T {
+        &mut self.elem
+    }
+}
+
+impl<T: ?Sized> Borrow<T> for NoisyDropper<T> {
+    fn borrow(&self) -> &T {
+        &self.elem
+    }
+}
+
+impl<T: ?Sized> BorrowMut<T> for NoisyDropper<T> {
+    fn borrow_mut(&mut self) -> &mut T {
+        &mut self.elem
+    }
+}
+
+impl<T: ?Sized> Deref for NoisyDropper<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.elem
+    }
+}
+
+impl<T: ?Sized> DerefMut for NoisyDropper<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.elem
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct DropNotifier {
+    dropped: AtomicBool,
+}
+
+impl DropNotifier {
+    pub(crate) fn new() -> Self {
+        Self {
+            dropped: AtomicBool::new(false),
+        }
+    }
+
+    pub(crate) fn was_dropped(&self) -> bool {
+        self.dropped.load(Ordering::Relaxed)
+    }
+}
+
+pub(crate) fn run_deferred() {
+    for _ in 0..65536 {
+        crossbeam_epoch::pin().flush();
+    }
+}
